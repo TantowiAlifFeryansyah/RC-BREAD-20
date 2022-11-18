589a2895593444dde707816968c4582632e2aa66
Tantowi Alif Feryansyah <tantowialif15@gmail.com>
2022-11-18 14:08:28 +0700

    Pagination

--- server.js
+++ server.js
@@ -22,10 +22,28 @@ app.set('view engine', 'ejs')
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+// app.get('/', (req, res) => {
+//     db.all(`SELECT * FROM Users`, (err, rows) => {
+//         if (err) return console.log('gagal ambil data', err);
+//         res.render('index', { dataUser: rows, moment })
+//     })
+// })
+
 app.get('/', (req, res) => {
-    db.all(`SELECT * FROM Users`, (err, rows) => {
-        if (err) return console.log('gagal ambil data', err);
-        res.render('index', { dataUser: rows, moment })
+    const page = req.query.page || 1
+    const limit = 3;
+    // const offset = 0
+    const offset = (page - 1) * limit
+
+    db.all(`SELECT count(*) As total from Users`, (err, data) => {
+        if (err) return console.log('gagal hitung data', err);
+        const total = data[0].total
+        const totalPages = Math.ceil(total / limit)
+        // console.log(totalPages);
+        db.all(`SELECT * FROM Users LIMIT ? offset ?`, [limit, offset], (err, rows) => {
+            if (err) return console.log('gagal ambil data', err);
+            res.render('index', { dataUser: rows, page, moment, totalPages })
+        })
     })
 })
 
@@ -51,11 +69,10 @@ app.get('/delete/:id', (req, res) => {
 
 app.get('/edit/:id', (req, res) => {
     const index = req.params.id
-    db.all(`SELECT * FROM Users WHERE ID = ?`,[index],(err, rows) => {
+    db.all(`SELECT * FROM Users WHERE ID = ?`, [index], (err, rows) => {
         if (err) return console.log('gagal ambil data', err);
         rows.forEach((item) => {
-            res.render('edit', {item})
-            // console.log(item);
+            res.render('edit', { item })
         })
     })
 })
@@ -69,11 +86,7 @@ app.post('/edit/:id', (req, res) => {
     })
 })
 
-// app.post('/edit/:id', (req, res) => {
-//     const index = req.params.id
-//     data[index] = {string: req.body.string, integer: Number(req.body.integer), float: parseFloat(req.body.float), date: req.body.date, boolean: JSON.parse(req.body.boolean) }
-//     res.redirect('/')
-// })
+
 
 
 app.listen(port, () => {
